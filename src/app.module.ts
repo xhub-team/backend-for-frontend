25fb14c6d52d20b6a292a039ffc8d4214876b37d
lindenes <dodigrams@gmail.com>
2025-03-05 19:18:42 +0300

    add dependencies(pg maybe depricated)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './application/service/controller';
-import { AppService } from './application/service/handler';
 import { ConfigService } from './configuration/service';
 import { UserTableService } from './infrastructure/service/user';
 import { DatabaseModule } from './infrastructure/module';
@@ -8,7 +7,7 @@ import { DatabaseModule } from './infrastructure/module';
 @Module({
   imports: [DatabaseModule],
   controllers: [AppController],
-  providers: [AppService, ConfigService, UserTableService],
+  providers: [ConfigService, UserTableService],
   exports: [ConfigService],
 })
 export class AppModule {}
